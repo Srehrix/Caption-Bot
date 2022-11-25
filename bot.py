56738c80b8998ae5d727a551dafe2800c0f2503e
Sree Hari <98310399+Srehrix@users.noreply.github.com>
2022-11-25 20:08:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,7 +23,30 @@ class autocaption(Client):
             plugins = dict(
                 root="Plugins"
             )
-        )
+     async def start(self):
+       await super().start()
+       me = await self.get_me()
+       self.mention = me.mention
+       self.username = me.username 
+       self.force_channel = FORCE_SUB
+       if FORCE_SUB:
+         try:
+            link = await self.export_chat_invite_link(FORCE_SUB)                  
+            self.invitelink = link
+         except Exception as e:
+            logging.warning(e)
+            logging.warning("Make Sure Bot admin in force sub channel")             
+            self.force_channel = None
+       app = web.AppRunner(await web_server())
+       await app.setup()
+       bind_address = "0.0.0.0"
+       await web.TCPSite(app, bind_address, PORT).start()
+       logging.info(f"{me.first_name} 𝚂𝚃𝙰𝚁𝚃𝙴𝙳 ⚡️⚡️⚡️")
+      
+
+    async def stop(self, *args):
+      await super().stop()      
+      logging.info("Bot Stopped")
 
 if __name__ == "__main__" :
     autocaption().run()
