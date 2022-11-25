4af10925f7810bbfe96080d39727d4d7d8ba1a25
Sree Hari <98310399+Srehrix@users.noreply.github.com>
2022-11-25 20:15:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ class Config(object):
       BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
       API_ID = int(os.environ.get("APP_ID", 12345))
       API_HASH = os.environ.get("API_HASH")
+      FORCE_SUB = os.environ.get("FORCE_SUB", "") 
+      FLOOD = int(os.environ.get("FLOOD", "10"))
       CAPTION_TEXT = os.environ.get("CAPTION_TEXT", "")
       CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "nil")
       ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "Sreehari3")
